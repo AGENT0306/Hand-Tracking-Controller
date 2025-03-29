24677620653b389df703d2246c7fcc3870a19457
AGENT0306 <reitrebicka74@gmail.com>
2025-03-28 21:49:36 -0400

    finished up creating the hand_class

--- hand_class.py
+++ hand_class.py
@@ -1,4 +1,6 @@
+import cv2
 import mediapipe as md
+
 
 class Hand:
     def __init__(self):
@@ -10,3 +12,39 @@ class Hand:
             min_tracking_confidence=0.5
         )
         self.mp_draw = md.solutions.drawing_utils
+
+    def find_hands(self, frame, draw=True):
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(rgb_frame)
+
+        if self.results.multi_hand_landmarks:
+            for hand_landmarks in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mp_draw.draw_landmarks(
+                        frame,
+                        hand_landmarks,
+                        self.mp_hands.HAND_CONNECTIONS
+                    )
+                    anchor_point = hand_landmarks.landmark[0]
+
+                    for id, lm in enumerate(hand_landmarks.landmark):
+                        h, w, c = frame.shape
+                        cx, cy, cz = int(lm.x * w), int(lm.y * h), int(lm.z)
+                        # Draw circle and text for each landmark
+                        cv2.circle(frame, (cx, cy), 3, (255, 0, 0), cv2.FILLED)
+                        cv2.putText(frame, str(cx - int(anchor_point.x * w)) + " " + str(int(anchor_point.y * h) - cy) + " " + str(id), (cx + 10, cy + 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+        final = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        return final
+
+    def find_position(self, frame, hand_no=0):
+        landmark_list = []
+        if self.results.multi_hand_landmarks:
+            if len(self.results.multi_hand_landmarks) > hand_no:
+                hand = self.results.multi_hand_landmarks[hand_no]
+                h, w, c = frame.shape
+                for id, lm in enumerate(hand.landmark):
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    landmark_list.append([id, cx, cy])
+        return landmark_list
+
--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import cv2 as cv
 import os
 import mediapipe as mp
+import hand_class as hand
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
-    static_image_mode=False,
-    max_num_hands=2,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
-)
-mp_draw = mp.solutions.drawing_utils
+hand_dect = hand.Hand()
 
 cap = cv.VideoCapture(0)
+
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
 
 if not cap.isOpened():
     print("ERROR!! Camera not opening!!")
@@ -26,30 +23,12 @@ while True:
 
     rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
-    results = hands.process(rgb_frame)
+    results = hand_dect.find_hands(rgb_frame)
 
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_draw.draw_landmarks(
-                frame,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS
-            )
-
-            for id, lm in enumerate(hand_landmarks.landmark):
-                h, w, c = frame.shape
-                # Convert normalized coordinates to pixel coordinates
-                cx, cy, cz= int(lm.x * w), int(lm.y * h), int(lm.z)
-                # Draw circle and text for each landmark
-                cv.circle(frame, (cx, cy), 3, (255, 0, 0), cv.FILLED)
-                cv.putText(frame, str(cx)+" "+str(cy)+" "+str(cz), (cx + 10, cy + 10),
-                cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-
-    cv.imshow("Video", frame)
+    cv.imshow("Video", results)
 
     if cv.waitKey(1) == 27:
         break
 
 cap.release()
 cv.destroyAllWindows()
-hands.close()
